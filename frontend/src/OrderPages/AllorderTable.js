ab8d230d2a1c41c9e9153b54cbb50c58ad4e3d18
Tisha Verma <tishav819@gmail.com>
2022-06-03 11:17:29 +0530

    make it better

--- frontend/src/OrderPages/AllorderTable.js
+++ frontend/src/OrderPages/AllorderTable.js
@@ -1,9 +1,31 @@
-import React from 'react'
 
-export default function AllorderTable() {
-  return (
-    <div>
-      
-    </div>
-  )
+
+
+import React, { useState } from 'react';
+import Popuplist from './Popuplist';
+
+export default function AllorderTable({ele}) {
+    const [isopenlist, setisopenlist] = useState(false);
+    function handlesummary(){
+        setisopenlist(!isopenlist)
+    }
+
+    
+    
+  return <div>
+      <div className='table-list-data' onClick={handlesummary}>
+            <p className='Order-Id '>{ele._id}</p>
+              <p className='date-time '>{ele.createdAt}</p>
+              <p className='store-location '>JP Nagar</p>
+              <p className='city'>Bengaluru</p>
+              <p className='store-phone '>9123456789</p>
+              <p className='total-items '>{ele.totalItems}</p>
+              <p className='Total-Price '>{ele.totalPrice}</p>
+              <p className='status '>ready to deliver</p>
+              <p className='view '><i className='fa fa-eye'></i></p>
+                </div>
+
+        {isopenlist ? 
+       <Popuplist finaldata={ele.orderItems} handleClose={handlesummary} />  :"" }
+  </div>;
 }
--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 
 export default function Footer() {
-  return <div>
+  return <div className='footer'>
+      <p>2022 &copy; Laundry </p>
 
   </div>;
 }
